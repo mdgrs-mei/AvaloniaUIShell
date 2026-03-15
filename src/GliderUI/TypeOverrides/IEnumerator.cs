925429fc91e7897ca256dcd644d2ce9b74837839
mdgrs <81177095+mdgrs-mei@users.noreply.github.com>
2026-03-15 20:41:57 +0900

    Override IEnumerator_Impl<T>.Current

---
+++ src/GliderUI/TypeOverrides/IEnumerator.cs
@@ -0,0 +1,19 @@
+﻿using GliderUI.Generator;
+
+namespace GliderUI.System.Collections.Generic;
+
+#pragma warning disable CA1063 // Implement IDisposable Correctly
+#pragma warning disable CA1707 // Identifiers should not contain underscores
+public partial class IEnumerator_Impl<T>
+#pragma warning restore CA1707
+#pragma warning restore CA1063
+{
+    // PowerShell calls the non-generic version of Current when it enumerates objects.
+    // When a private type is returned, since the return type is object and there is no type mapping avalable, it throws.
+    // Always use the typed version of Current so that it can create the corresponding type.
+    [SurpressGeneratorPropertyByName]
+    object global::System.Collections.IEnumerator.Current
+    {
+        get => Current!;
+    }
+}
